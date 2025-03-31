5811dc227d5d8be7613634481b801fc80caa28bb
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2025-03-31 01:12:17 -0300

    - Salvando as credenciais do usuário em cache - Filtrando os produtos conforme a categoria

--- src/resources/screens/product_list/ProductListScreen.js
+++ src/resources/screens/product_list/ProductListScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { ScrollView, Alert } from "react-native";
 import { styles } from "./ProductListScreenStyle";
 import {
   findProductsByCategory,
@@ -7,6 +7,7 @@ import {
   findAllProdutos,
 } from "../../../app/db/ProdutoDao";
 import { ProductSaleCard } from "../../components/product_sale_card/ProductSaleCard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function ProductListScreen({ navigation, route }) {
   const { categoriaId, productName } = route.params || {};
@@ -33,13 +34,42 @@ export default function ProductListScreen({ navigation, route }) {
     }
   }
 
+  async function addOnCart(produto) {
+    try {
+      let index = carrinho.findIndex((item) => item.codigo == produto.codigo);
+
+      if (index == -1) {
+        let obj = {
+          codigo: produto.codigo,
+          imagem: produto.imagem,
+          descricao: produto.descricao,
+          preco: produto.preco,
+        };
+        carrinho.push(obj);
+
+        let objString = JSON.stringify(lista);
+        await AsyncStorage.setItem(chaveCarrinho, objString);
+        Alert.alert("Sucesso", "Item adicionado ao carrinho!");
+
+        await carregaCarrinho();
+      } else {
+        Alert.alert("Lembrete", "Produto já está no carrinho");
+      }
+    } catch (e) {
+      Alert.alert(e.toString());
+    }
+  }
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={{ gap: 8, alignItems: "center" }}
     >
       {products.map((product, index) => (
-        <ProductSaleCard key={index} produto={product} />
+        <ProductSaleCard
+          key={index}
+          produto={product}
+          adicionaCarrinho={addOnCart}
+        />
       ))}
     </ScrollView>
   );
