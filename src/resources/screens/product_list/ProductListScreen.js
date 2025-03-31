e7feb3b0bf2ec606c68c8b0d3a4527a46a0eaeee
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2025-03-31 01:04:04 -0300

    Filtro de texto e por modalidade

--- src/resources/screens/product_list/ProductListScreen.js
+++ src/resources/screens/product_list/ProductListScreen.js
@@ -1,14 +1,46 @@
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { styles } from "./ProductListScreenStyle";
+import {
+  findProductsByCategory,
+  findProductsByName,
+  findAllProdutos,
+} from "../../../app/db/ProdutoDao";
+import { ProductSaleCard } from "../../components/product_sale_card/ProductSaleCard";
 
 export default function ProductListScreen({ navigation, route }) {
-  // Acesse os parâmetros usando route.params
-  const { categoriaId } = route.params || {};
+  const { categoriaId, productName } = route.params || {};
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    initScreen();
+  }, []);
+
+  async function initScreen() {
+    try {
+      var products = [];
+
+      if (categoriaId) {
+        products = await findProductsByCategory(categoriaId);
+      } else if (productName) {
+        products = await findProductsByName(productName);
+      } else {
+        products = await findAllProdutos();
+      }
+      setProducts(products);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível carregar os produtos");
+    }
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Lista de produtos</Text>
-      {categoriaId && <Text>Parâmetro recebido: {categoriaId}</Text>}
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{ gap: 8, alignItems: "center" }}
+    >
+      {products.map((product, index) => (
+        <ProductSaleCard key={index} produto={product} />
+      ))}
+    </ScrollView>
   );
 }
